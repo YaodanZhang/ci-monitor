cd962a9eaf77da89dd9e9ff7c27035a7095a5cc0
Davenkin <davenkin@gmail.com>
2013-05-20 12:52:04 +0800

    added email handler for jenkins

---
+++ src/main/java/davenkin/cimonitor/email/JenkinsEmailHandler.java
@@ -0,0 +1,29 @@
+package davenkin.cimonitor.email;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 5/17/13
+ * Time: 8:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JenkinsEmailHandler extends AbstractEmailHandler {
+
+    @Override
+    public boolean canHandle(MimeMessage mimeMessage) throws MessagingException {
+        return mimeMessage.getSubject().contains("Jenkins");
+    }
+
+    protected String resolveProjectName(MimeMessage mimeMessage) throws MessagingException {
+        return mimeMessage.getSubject().split(":|#")[1].trim();
+    }
+
+    protected boolean resolveProjectStatus(MimeMessage mimeMessage) throws MessagingException {
+        String subject = mimeMessage.getSubject();
+        return !subject.contains("failed");
+    }
+
+}

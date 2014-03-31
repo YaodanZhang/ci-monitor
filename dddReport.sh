fa184b581d5f99a42766ad41ad970702bbf0c624
davenkin <davenkin@gmail.com>
2014-03-31 12:32:24 +0800

    Revert "amazon reporting for IDDD"
    This reverts commit 371880b3472e86a89cfcef5d585fbde0f9e50164.

--- dddReport.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-var=`curl http://www.amazon.cn/gp/bestsellers/books/660993051/ref=pd_zg_hrsr_b_1_4_last | grep "title=\"实现领域驱动设计\"" | grep zg_rankNumber | egrep -o '[0-9]\.<' | cut -d "." -f1`
-echo $(date +"%m-%d-%Y"),$var >> aa.txt
